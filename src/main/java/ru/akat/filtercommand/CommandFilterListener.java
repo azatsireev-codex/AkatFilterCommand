767b6a43feef38cfcd7584985382168ebae573fd
Akat <azat.sireev@gmail.com>
2026-02-21 16:54:58 +0300

    Filter Brigadier command tree for tab completion

--- src/main/java/ru/akat/filtercommand/CommandFilterListener.java
+++ src/main/java/ru/akat/filtercommand/CommandFilterListener.java
@@ -1,11 +1,14 @@
 package ru.akat.filtercommand;
 
+import com.mojang.brigadier.tree.RootCommandNode;
+import io.papermc.paper.event.brigadier.AsyncPlayerSendCommandsEvent;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerCommandSendEvent;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 
 public final class CommandFilterListener implements Listener {
@@ -22,6 +25,18 @@ public final class CommandFilterListener implements Listener {
         for (String root : new HashSet<>(event.getCommands())) {
             if (!commandResolver.canUse(player, root)) {
                 event.getCommands().remove(root);
+            }
+        }
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    public void onAsyncCommandTreeSend(AsyncPlayerSendCommandsEvent<?> event) {
+        Player player = event.getPlayer();
+        RootCommandNode<?> rootNode = event.getCommandNode();
+
+        for (String root : new ArrayList<>(rootNode.getChildren().stream().map(node -> node.getName()).toList())) {
+            if (!commandResolver.canUse(player, root)) {
+                rootNode.removeCommand(root);
             }
         }
     }
